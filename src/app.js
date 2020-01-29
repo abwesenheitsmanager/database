022678df5ea20a3918cf28d3d0334573d36d9608
Benjamin Bulis <bbulis@student.tgm.ac.at>
2020-01-29 14:12:36 +0100

    Snapshot Commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import sequelize from "./config/database.config";
+import {buildResponse} from "./helper/response";
 
 /**
  * Config for dotenv to find the .env file
@@ -10,10 +12,19 @@ dotenv.config({ path: path.join(process.cwd(), '../.env')});
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+async function testDatabase() {
+    try {
+        await sequelize.authenticate();
+        console.log(buildResponse(true, "Database server is running"))
+    } catch (e) {
+        console.log(buildResponse(false, e));
+    }
+}
 
 /**
  * Starting listening on this PORT
  */
 app.listen(PORT, () => {
     console.log(`Server ist running on port ${PORT}...`);
+    testDatabase();
 });
