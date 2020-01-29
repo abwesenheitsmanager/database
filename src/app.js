f78ad48aa1887c1f0c297c5726e48000c741b855
Benjamin Bulis <bbulis@student.tgm.ac.at>
2020-01-29 12:02:24 +0100

    Fixed Issues with dotenv and babel Convertion

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path'
+
+dotenv.config({ path: path.join(process.cwd(), '../.env')});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
 
 app.listen(PORT, () => {
     console.log(`Server ist running on port ${PORT}...`);
