4194b236dfa8429b9c3a4f9fb792fd8f777d13ce
Benjamin Bulis <bbulis@student.tgm.ac.at>
2020-02-11 15:05:29 +0100

    Edit Dienstreise Model

--- readme.md
+++ readme.md
@@ -2,3 +2,7 @@
 ## Abwesenheitsmanager Projekt
 ### Einführung
 Verbindung zur Datenbank und bereitstellung der Daten. 
+
+### Speicherung von Daten
+Eine Auswhal von Daten wird als ein großer String gespeichert. Die Daten
+werden durch den Einsatz von Sonderzeichen getrennt. 
--- src/models/dienstreise.js
+++ src/models/dienstreise.js
@@ -1,9 +1,52 @@
 import Sequelize from "sequelize";
 import sequelize from "../config/database.config";
+import {User} from "./user";
 
 class Dienstreise extends Model() {}
 Dienstreise.init({
-    
+    ausland: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+    },
+    reiseziel: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    dienstreise_beginn: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    dienstreise_ende: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    dienst_beginn: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    dienst_ende: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    reisezweck: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    reisemittel: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    ausgangspunkt: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    endpunkt: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
 }, {sequelize, modelName: "dienstreise"});
+
+User.hasMany(Dienstreise);
+Dienstreise.hasMany(User);
 
 export {Dienstreise};
